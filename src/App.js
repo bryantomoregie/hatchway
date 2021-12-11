6ca09f749566f5d527226feebb9e16de699f5301
Bryant Omoregie <unclebryant@gmail.com>
2021-12-11 07:55:07 -0600

    FIlter by first name working

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Student } from "./Student";
 import { Search } from "./Search";
 import "./styles/app.css";
 
 const App = () => {
-  let [students, setStudents] = useState([]);
-  let originalStudents;
-  const [name, setName] = useState();
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState("");
   const [tag, setTag] = useState();
 
   useEffect(() => {
     fetch("https://api.hatchways.io/assessment/students")
       .then((response) => response.json())
       .then((data) => setStudents(data.students));
-    originalStudents = students;
   }, []);
 
-  console.log(originalStudents, students);
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-  useEffect(() => {
-    setStudents(
-      students.slice().filter((student) => {
-        return student.firstName.includes(name);
-      })
-    );
-  }, [name]);
+  const filteredStudents = useMemo(() => {
+    let copyOfStudentArray = students;
+
+    if (name === "") {
+      return copyOfStudentArray;
+    }
+
+    const capName = capitalizeFirstLetter(name);
+
+    const filterWithCaps = copyOfStudentArray.filter((student) => {
+      return student.firstName.includes(capName);
+    });
+
+    const filterWithoutCaps = copyOfStudentArray.filter((student) => {
+      return student.firstName.includes(name);
+    });
+
+    const arrayConcat = filterWithCaps.concat(filterWithoutCaps);
+
+    let uniqueFilter = arrayConcat.filter((c, index) => {
+      //come back to this
+      return arrayConcat.indexOf(c) === index;
+    });
+
+    return uniqueFilter;
+  }, [students, name]);
 
   return (
     <div className="container">
       <Search name={name} setName={setName} tag={tag} setTag={setTag} />
       <div>
-        {students.map((student) => (
+        {filteredStudents.map((student) => (
           <Student key={student.id} {...student} />
         ))}
       </div>
